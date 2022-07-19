725b0fbf348b6369a261c26f8ac2e45c1e4f525b
SaraGarcesCespedes <sgarcesc@unal.edu.co>
2022-07-18 21:23:40 -0500

    nuevo modulo introduccion

--- Modulo 3/Diseño de bloques completos aleatorizados.R
+++ Modulo 3/Diseño de bloques completos aleatorizados.R
@@ -26,3 +26,66 @@ ggplot(data = df, aes(x = tiempo, y = metodo, fill = metodo)) +
 # Pruebas múltiples
 pruebas_multiples <- LSD.test(anova, "metodo")
 pruebas_multiples
+
+# Verificacion de supuestos
+# VERIFICACION DE SUPUESTOS
+
+# calcular los residuos
+residuos <- residuals(anova)
+residuos
+
+# promedio de moscas muertas por tratamiento
+prom_tratamientos <- df %>% group_by(marca) %>%
+  summarise(prom_moscas = mean(numero_moscas))
+prom_tratamientos
+
+df$residuos <- residuos
+df <- df %>% left_join(prom_tratamientos, by = "marca")
+
+# histograma de residuos
+ggplot(df, aes(x=residuos))+
+  geom_histogram(breaks = seq(-12, 12, 3), fill="#702899", color="black", bins = 10) +
+  labs(y = "Frecuencia") + 
+  scale_x_continuous(breaks = seq(-12, 12, 3))
+
+# gráfico qqplot
+EnvStats::qqPlot(residuos, #Un gráfico Cuantil-Cuantil de los residuos
+                 pch =20, #Forma de los puntos
+                 main="QQ-Plot de los residuos", #Título principal
+                 xlab = "Cuantiles teóricos",  #Etiqueta eje x
+                 ylab="Cuantiles observados de los residuos",
+                 add.line = TRUE) #Etiqueta eje y
+
+# Prueba shapiro-wilks
+shapiro.test(residuos) 
+
+# Supuesto de varianza constante
+
+# gráfica de valores estimados versus residuos
+ggplot(data = df, aes(x = prom_moscas, y = residuos)) +
+  geom_point() + 
+  theme_bw() +
+  labs(y = "Residuos",
+       x = "Valores estimados") +
+  geom_abline(intercept = 0, slope = 0, color='red') 
+
+
+# prueba de Bartlett
+bartlett.test(numero_moscas ~ marca, data = df)
+
+
+# supuesto de independencia
+ggplot(data = df, aes(x = orden_pruebas, y = residuos)) +
+  geom_point() + 
+  geom_line() +
+  theme_bw() +
+  labs(y = "Residuos",
+       x = "Orden corridas experimentales") +
+  geom_abline(intercept = 0, slope = 0, color='red') 
+
+# prueba Durbin-Watson
+durbinWatsonTest(anova) 
+
+
+# otra forma de construir las gráficas
+plot(anova)
